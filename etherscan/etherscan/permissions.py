2074996ba117f43db6565d97696d1eb9e8706250
Mauricio <mauricioblancoregojo@gmail.com>
2021-08-29 18:55:01 -0300

    validation token added

---
+++ etherscan/etherscan/permissions.py
@@ -0,0 +1,7 @@
+from rest_framework import permissions
+from django.conf import settings
+
+
+class IsAllowed(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.META.get("HTTP_ACCESS_TOKEN") == settings.DEFAULT_ETHERSCAN_API_TOKEN
